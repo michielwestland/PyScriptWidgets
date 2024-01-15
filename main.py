393147b54e70b3abb0eb0decc304fa0b6f580765
Michiel Westland <michielwestland@gmail.com>
2024-01-15 17:26:16 +0100

    Async webservice requestr in clock handler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from js import console, document, sessionStorage, fetch, JSON, window
 from pyodide.ffi import create_proxy
@@ -9,25 +10,31 @@ def dump(obj):
         if attr.startswith("v"):
             display(attr + " = " + repr(getattr(obj, attr)))
 
-def main():
+async def main():
     time = ""
     root = document.getElementById("root")
 
     txIn = document.createElement("input")
     txIn.setAttribute("placeholder", "press this button -->") # attribute
-    txIn.setAttribute("style", "width: 300px")
+    txIn.setAttribute("style", "width: 500px") # css
     root.appendChild(txIn)
     dump(txIn)
 
     btn = document.createElement("button")
     btn.appendChild(document.createTextNode("Press me!")) # text
-    def btn_click(*args):
+    async def btn_click(*args): # event handler
+        btn.setAttribute("style", "color: red")
         console.log(repr(args))
         time = str(datetime.now())
+
+        response = await fetch("pyscript.json", {"method": "GET"}) # webservice request
+        data = await response.json()
+        time = time + " " + JSON.stringify(data)
+
         sessionStorage.setItem("now", time) # serialize the class tree using JSON.stringify(data)
         txIn.value = "Now is: " + time
+
     btn.addEventListener("click", create_proxy(btn_click)) # event
-    btn.setAttribute("style", "color: red")
     root.appendChild(btn)
 
     t = sessionStorage.getItem("now") # de-serialize the class tree using JSON.parse(data)
@@ -36,4 +43,4 @@ def main():
         txIn.value = "Loaded: " + time
 
 if __name__ == "__main__":
-    main()
+    asyncio.ensure_future(main())
